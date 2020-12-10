ccc144c62720070c7a8be1a955f4bdcc448400f9
Ericu03 <44855495+Ericu03@users.noreply.github.com>
2020-12-10 17:26:22 +0900

    Update auton_functions.h

--- auton_functions.h
+++ auton_functions.h
@@ -2,9 +2,8 @@
 #include "config.hpp"
 
 void slewRateControl(pros::Motor *motor, int targetVelocity, int increment);
-void base_PID(float targetdistance, int velocity);
+void base_PID(double targetdistance, int velocity);
 void turn_PID(float targetdegree);
-void newbase_PID(int distance, int max_velocity);
-void newturn_PID(float targetDegree);
+
 void brakeMotor();
 void unbrakeMotor();
