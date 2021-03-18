3ef6fcddf8cce6e776fe326ef5a88916dce896ea
Ericu03 <44855495+Ericu03@users.noreply.github.com>
2021-03-18 16:28:37 +0900

    Update autonomous.cpp

--- autonomous.cpp
+++ autonomous.cpp
@@ -2,46 +2,16 @@
 #include "config.hpp"
 #include "auton_functions.h"
 
-void blueleft(){
-base_PID(1,20);
-pros::delay(300);
-
-//turn_PID(90);
 
 
-tlw.move_velocity(0);
-trw.move_velocity(0);
-}
-void blueright(){
-	intakeL.move_velocity(600);
-	intakeR.move_velocity(600);
-	base_PID(5.0,50);
-	pros::delay(300);
-	turn_PID(45);
-	pros::delay(100);
-	base_PID(3.0,30);
-	intakeL.move_velocity(400);
-	intakeR.move_velocity(400);
-	rollT.move_velocity(550);
-	rollB.move_velocity(550);
-	pros::delay(50);
-}
-
-void redleft(){
-intakeL.move_velocity(600);
-intakeR.move_velocity(600);
-pros::delay(500);
-intakeL.move_velocity(0);
-intakeR.move_velocity(0);
-}
-
-void redright(){
-turn_PID(90);
-
-pros::delay(10);
-}
 
 void autonomous() {
+base_PID(10,90);
+pros::delay(10);
+turn_PID(45);
+
+
+/**
 
 if(sideautonstate == 2 && colorautonstate ==2){
 	blueleft();
@@ -55,6 +25,15 @@ if(sideautonstate ==  2 && colorautonstate == 1){
 }
 if(sideautonstate == 1 && colorautonstate == 1){
 	redright();
-}
+}**/
 
 }
+
+
+/**leftenc.reset();
+rightenc.reset();
+while(true){
+	printf("Left %f\n",(double)leftenc.get_value());
+	printf("Right %f\n",(double)rightenc.get_value());
+	pros::delay(5);
+}**/
